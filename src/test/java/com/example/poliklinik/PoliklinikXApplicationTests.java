c2fd269d7377768030310dcca4b43952283d69c0
unknown <NXAZ6SN0031510AA023400@outlook.com>
2024-12-04 12:20:58 +0700

    initial spring & server config

---
+++ src/test/java/com/example/poliklinik/PoliklinikXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.poliklinik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoliklinikXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
